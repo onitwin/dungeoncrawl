2721ac171070f6a4ce29fe9b582cfb9044b34c59
onitwin <onitwin@gmail.com>
2021-03-03 10:30:05 +0000

    added room class

--- src/main/java/dungeon/Room.java
+++ src/main/java/dungeon/Room.java
@@ -1,4 +1,31 @@
 package dungeon;
 
+import behaviors.IUse;
+import characters.Monster;
+import characters.Player;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+
 public class Room {
+
+    private String roomname;
+    private String description;
+    private ArrayList<Monster> monsters;
+    private HashMap<String,Room> rooms;
+    private boolean visited;
+    private Player player;
+    private ArrayList<IUse>loot;
+
+    public Room (String roomname,String description){
+        this.roomname=roomname;
+        this.description=description;
+        this.player=null;
+        this.monsters=new ArrayList<Monster>();
+        this.loot=new ArrayList<IUse>();
+        this.visited=false;
+        this.rooms=new HashMap<String, Room>();
+    }
+
+
 }
